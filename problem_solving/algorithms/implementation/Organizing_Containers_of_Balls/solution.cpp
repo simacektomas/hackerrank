46c1ff60ebd4991d44dd27b7a91e3477552f3ab6
Tomas Simacek <tomas.simacek@oracle.com>
2022-04-25 21:47:17 +0200

    Implementation: Implement O(n) solution for Organizing Containers of Balls

--- problem_solving/algorithms/implementation/Organizing_Containers_of_Balls/solution.cpp
+++ problem_solving/algorithms/implementation/Organizing_Containers_of_Balls/solution.cpp
@@ -1,25 +1,24 @@
 string organizingContainers(vector<vector<int>> container) {
-    vector<int> ballonTypeSum; 
-    vector<int> containerSum; 
+    multiset<int> ballonTypeSum; 
+    multiset<int> containerSum; 
     
     for (vector<int> & c: container) {
         // calculate sums for each container;
         int sum = 0;
         for(int i: c) sum += i;
-        containerSum.insert(lower_bound(containerSum.begin(), containerSum.end(), sum), sum);
+        containerSum.insert(sum);
     }
     
     for (int i = 0; i < container.size(); i++) {
         // calculate sums for each container;
         int sum = 0;
         for(int y = 0; y < container.size(); y++) sum += container[y][i];
-        ballonTypeSum.insert(lower_bound(ballonTypeSum.begin(), ballonTypeSum.end(), sum), sum);
+        ballonTypeSum.insert(sum);
     }
     
     //sort(ballonTypeSum.begin(), ballonTypeSum.end());
     //sort(containerSum.begin(), containerSum.end());
     
-    for(int i = 0; i < container.size(); i++)
-        if(ballonTypeSum[i] != containerSum[i]) return "Impossible";
+    if(ballonTypeSum != containerSum) return "Impossible";
     return "Possible";
 }
